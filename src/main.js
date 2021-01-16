8de7141b5677607aad146a080b605ba24b4eb936
jiang12345678910 <2233579357@qq.com>
2021-01-16 17:20:22 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
+/**
+ * 项目的启动入口
+ */
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// 加载全局样式文件
+import './styles/index.less'
 
 createApp(App).use(router).mount('#app')
